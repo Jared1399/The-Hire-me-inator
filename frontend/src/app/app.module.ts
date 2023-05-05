de9e660df4c2bffe45b2e50f7af02b11ab51db8f
Jared Quiero Astudillo <53412039+Jared1399@users.noreply.github.com>
2023-05-05 13:33:48 -0400

    Revert "added CORS configuration for backend and frontend communication, API …"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,23 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './pages/landing/landing.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule { }
